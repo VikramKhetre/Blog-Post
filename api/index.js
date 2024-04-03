010d08df4d2f9ead5cb47a699bdd8675be403c6d
VikramKhetre <khetre.vikram@gmail.com>
2024-04-03 19:08:50 +0530

    connect to db mongo atlas

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import  express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then((res)=>{
+    console.log("db is connected sucessfully")
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
 app.listen(8080,()=>{
     console.log("Listinging at 8080!");
+})
+
+app.use("/",()=>{
+    console.log("use is working")
 })
