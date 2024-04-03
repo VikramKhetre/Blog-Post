7c05261699587324be365c5fa2386ceec8507ac2
VikramKhetre <khetre.vikram@gmail.com>
2024-04-04 02:03:08 +0530

    created signup api route and tested inserting data with postman

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ import  express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js"
+import authRoute from "./routes/authRoute.js"
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-.then((res)=>{
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
     console.log("db is connected sucessfully")
 })
 .catch((err)=>{
@@ -15,8 +17,11 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(8080,()=>{
-    console.log("Listinging at 8080!");
+    console.log("Lisnging at 8080!");
 })
 
 app.use("/api",userRoute)
+app.use("/api/auth",authRoute)
