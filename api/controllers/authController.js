dc600357b746e8ce6ea354c52a49038d860d370e
VikramKhetre <khetre.vikram@gmail.com>
2024-04-05 19:49:58 +0530

    signin api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,12 +1,16 @@
 import User from "../models/users.model.js";
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signup = async (req,res,next)=>{
 
     const {username , email, password} = req.body;
 
-    if(!username || !email || !password || username==="" || email===""){
+    if(!username || !email || !password || username==="" || email==="" || password===""){
         next(errorHandler(400,"All fields are required"));
         
     }
@@ -26,4 +30,40 @@ export const signup = async (req,res,next)=>{
     } catch(error){
        next(error)
     }
+}
+
+export const signin = async(req,res,next)=>{
+    const {email, password} = req.body;
+
+    if(!email || !password || email==="" ||password===""){
+        next(errorHandler(400,"All fields are required"));
+        
+    }
+
+    try{    
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"invalid credentials"))
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400,"invalid credentials"))
+        }
+
+        const token = jwt.sign(
+            {id:validUser._id},process.env.JWT_SECRET
+        );
+
+        // to hide password from sharing back to res
+        const {password: pass, ...rest}=validUser._doc
+
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+     // here we pass rest to hide password
+        }).json(rest)
+
+    }catch(error){
+        next(error)
+    }
+
 }
