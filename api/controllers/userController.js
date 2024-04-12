c6d6c412a688d5102dd1ff1fa4bdd3d7afb93764
VikramKhetre <khetre.vikram@gmail.com>
2024-04-12 21:09:54 +0530

    added delte api route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -48,3 +48,15 @@ export const updateUser = async (req,res,next)=>{
         next(error)
     }
 }
+
+export const deleteUser = async (req,res,next)=>{
+    if(req.user.id !== req.params.userId){
+        return next(errorHandler(403, "u are not allowed to update this user"));
+    }
+    try{
+        await User.findByIdAndDelete(req.params.userId)
+        res.status(200).json({message:'User deleted Successfully'});
+    }catch(error){
+        next(error)
+    }
+}
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 const router = express.Router();
-import {test,updateUser} from "../controllers/userController.js"
+import {test,updateUser,deleteUser} from "../controllers/userController.js"
 import {verifyToken} from "../utils/verifyUser.js"
 
 router.get("/test",test)
 router.put("/user/update/:userId",verifyToken,updateUser)
+router.delete("/user/delete/:userId",verifyToken,deleteUser)
 
 export default router
