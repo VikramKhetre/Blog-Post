e9daf64fa5b359f054ee614fcf1c6e2c5ba9a7a0
VikramKhetre <khetre.vikram@gmail.com>
2024-04-22 22:41:30 +0530

    add post section to dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Label, Sidebar } from "flowbite-react";
-import { HiUser, HiArrowSmRight } from "react-icons/hi";
+import { HiUser, HiArrowSmRight, HiDocumentText } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
@@ -37,17 +37,28 @@ export default function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
-              label={"User"}
+              label={currentUser.isAdmin ? 'Admin':'User'}
               as={"div"}
             >
               profile
             </Sidebar.Item>
           </Link>
+          {currentUser.isAdmin &&(
+          <Link to="/dashboard?tab=posts">
+            <Sidebar.Item
+              active={tab === "posts"}
+              icon={HiDocumentText}
+              as={"div"}
+            >
+              Posts
+            </Sidebar.Item>
+          </Link>
+          )}
           <Sidebar.Item
             icon={HiArrowSmRight}
             lableColor="dark"
