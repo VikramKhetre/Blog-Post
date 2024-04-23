8a51fdefde0b31d99949dac0fc18c85b0618c1d5
VikramKhetre <khetre.vikram@gmail.com>
2024-04-23 11:48:31 +0530

    get posts api route

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -25,3 +25,53 @@ export const create = async (req,res,next)=>{
     next(error);
   }
 };
+
+export const getPosts = async (req,res,next)=>{
+  try{
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc'?1:-1;
+
+    // finding based on query 
+    const post = await Post.find({
+      ...(req.query.userId && {userId:req.query.userId}),
+      ...(req.query.category && {category:req.query.category}),
+      ...(req.query.slug && {slug:req.query.slug}),
+      ...(req.query.postId && {_id:req.query.postId}),
+      // searchTerm
+      ...(req.query.searchTerm && { 
+        $or : [
+              {title:{$regex:req.query.searchTerm,$options:'i'}},
+              {content:{$regex:req.query.searchTerm,$options:'i'}}
+              ]
+    })
+  }).sort(sortDirection).skip(startIndex).limit(limit);
+
+  // get total no of posts
+   const totalPost = await Post.countDocuments();
+
+  // total no of post in last months
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() -1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt:{$gte:oneMonthAgo}
+    })
+
+    // sending res
+    res.status(200).json({
+      post,
+      totalPost,
+      lastMonthPosts
+    });
+
+
+  }catch(error){
+    next(error)
+  }
+}
--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { create } from '../controllers/postController.js';
+import { getPosts } from '../controllers/postController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
+router.get('/getPosts',getPosts)
 
 export default router;
