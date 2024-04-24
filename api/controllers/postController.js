b7d650645fbf89a5534e11d51816457767982d0f
VikramKhetre <khetre.vikram@gmail.com>
2024-04-24 10:32:59 +0530

    delete post functionality

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,18 +1,18 @@
-import { errorHandler } from "../utils/error.js"
-import Post from "../models/post.model.js"
+import { errorHandler } from "../utils/error.js";
+import Post from "../models/post.model.js";
 
-export const create = async (req,res,next)=>{
-    if(!req.user.isAdmin){
-        return next(errorHandler(404,"you are not allowed to create post"))
-    }
-    if(!req.body.title || !req.body.content){
-        return next (errorHandler("404","please provide all required fields"))
-    }
-    const slug = req.body.title
-    .split(' ')
-    .join('-')
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(404, "you are not allowed to create post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler("404", "please provide all required fields"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
     .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, '');
+    .replace(/[^a-zA-Z0-9-]/g, "");
   const newPost = new Post({
     ...req.body,
     slug,
@@ -26,52 +26,65 @@ export const create = async (req,res,next)=>{
   }
 };
 
-export const getPosts = async (req,res,next)=>{
-  try{
+export const getPosts = async (req, res, next) => {
+  try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === 'asc'?1:-1;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
 
-    // finding based on query 
+    // finding based on query
     const post = await Post.find({
-      ...(req.query.userId && {userId:req.query.userId}),
-      ...(req.query.category && {category:req.query.category}),
-      ...(req.query.slug && {slug:req.query.slug}),
-      ...(req.query.postId && {_id:req.query.postId}),
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
       // searchTerm
-      ...(req.query.searchTerm && { 
-        $or : [
-              {title:{$regex:req.query.searchTerm,$options:'i'}},
-              {content:{$regex:req.query.searchTerm,$options:'i'}}
-              ]
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
     })
-  }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
 
-  // get total no of posts
-   const totalPost = await Post.countDocuments();
+    // get total no of posts
+    const totalPost = await Post.countDocuments();
 
-  // total no of post in last months
+    // total no of post in last months
     const now = new Date();
 
     const oneMonthAgo = new Date(
       now.getFullYear(),
-      now.getMonth() -1,
+      now.getMonth() - 1,
       now.getDate()
     );
 
     const lastMonthPosts = await Post.countDocuments({
-      createdAt:{$gte:oneMonthAgo}
-    })
+      createdAt: { $gte: oneMonthAgo },
+    });
 
     // sending res
     res.status(200).json({
       post,
       totalPost,
-      lastMonthPosts
+      lastMonthPosts,
     });
-
-
-  }catch(error){
-    next(error)
+  } catch (error) {
+    next(error);
   }
-}
+};
+
+export const deletePost = async (req, res, next) => {
+  try {
+  if(!req.user.isAdmin || req.user.id !== req.params.userId){
+    return next(errorHandler(403,'you are not allowed to delete this post'));
+  }
+  await Post.findByIdAndDelete(req.params.postId);
+  res.status(200).json('the post has been deleted')
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { create } from '../controllers/postController.js';
-import { getPosts } from '../controllers/postController.js';
+import { create,getPosts,deletePost } from '../controllers/postController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
 router.get('/getPosts',getPosts)
+router.delete('/deletePost/:postId/:userId',verifyToken,deletePost)
 
 export default router;
