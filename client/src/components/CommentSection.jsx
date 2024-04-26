863d41c0a0e13a4958eed2e70378946b99dcff58
VikramKhetre <khetre.vikram@gmail.com>
2024-04-26 23:49:29 +0530

    add like functionality to the comment

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,15 +1,15 @@
 import { Alert, Button, TextInput, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import Comment from './Comment';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
   const [commentError, setCommentError] = useState(null);
-  console.log(comments);
+  const navigate = useNavigate();
 
   useEffect(()=>{
     const getComments = async()=>{
@@ -51,6 +51,33 @@ export default function CommentSection({ postId }) {
       }
     } catch (error) {
       setCommentError(error.message);
+    }
+  };
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   };
   return (
@@ -116,7 +143,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment._id} comment={comment}onLike={handleLike} />
           ))}   
         </>
       )}
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,10 +21,10 @@ export default function Header() {
 
   const handleSignOut = async (e) => {
     try {
-      const res = await fetch(`api/user/signout`, {
+      const res = await fetch('/api/user/signout', {
         method: "POST",
       });
-      const data = res.json();
+      const data = await res.json();
       if (res.ok) {
         dispatch(signoutSuccess(data));
       } else {
