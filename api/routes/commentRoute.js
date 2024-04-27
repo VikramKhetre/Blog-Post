bac2a3599420af330f631b55d02c94dd931aaf5f
VikramKhetre <khetre.vikram@gmail.com>
2024-04-27 18:01:02 +0530

    delte comment functionality

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComment, likeComment,editComment } from '../controllers/commentController.js';
+import { createComment, getPostComment, likeComment,editComment, deleteComment } from '../controllers/commentController.js';
 
 const router = express.Router();
 
@@ -9,4 +9,5 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId',getPostComment)
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 export default router;
